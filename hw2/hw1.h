415334034e1371efeaaa6cf892c6af85c7dfddff
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-09-16 04:02:28 -0600

    Modified test.c and added reverse function code in that. Tested successfully. Also created a new folder which contains files of hw2.

---
+++ hw2/hw1.h
@@ -0,0 +1,4 @@
+#ifndef _HW1_H
+#define _HW1_H
+char reverse(char * str, int length);
+#endif
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdint.h>
-#include<memory.h>
-int8_t my_memzero(uint8_t *src,uint32_t length);
+#include"memory.h"
+int8_t my_reverse(char *src, int length);
 void main()
 {
     uint32_t length;
@@ -11,10 +11,39 @@ void main()
 	    printf("enter the array \t");
 		scanf ("%[^\n]%*c",source);
 
-    printf("Enter the number of bytes that you want to  \t");
+    printf("Enter the number of bytes that you want to reverse \t");
     scanf("%d",&length);
-    my_memzero(source,length);
+    my_reverse(source,length);
 }
 
 
+int8_t my_reverse(char *src, int length)
+{
+    uint16_t i=0,counter;
+    
+    if(src)
+     {
+    
+      int8_t temp;
+      for (counter=0;counter<(length/2);counter++)
+       {
+         temp=*(src+counter);
+         *(src+counter)=*(src+length-counter-1);
+         *(src+length-counter-1)=temp;
+       }
+       printf("\n Reveresed string is \t");
+       while(i<length)
+         {
+           printf("%c",*(src+i));
+           i++;
+         }
+	  return 0;
+       }
+    else
+       {
+         return 1;
+       }
+     
+
+}
 
