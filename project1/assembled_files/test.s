5306d8f09e0c5b754b8a1fe95878c4378c11b41e
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-09-17 22:53:13 -0600

    Created target asm-file for assembling either individual file or all files

---
+++ project1/assembled_files/test.s
@@ -0,0 +1,62 @@
+	.file	"test.c"
+	.section	.rodata
+.LC0:
+	.string	"enter the array \t"
+.LC1:
+	.string	"%[^\n]%*c"
+	.align 8
+.LC2:
+	.string	"Enter the number of bytes that you want to reverse \t"
+.LC3:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$80, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	leaq	-64(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movl	$.LC2, %edi
+	movl	$0, %eax
+	call	printf
+	leaq	-68(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC3, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movl	-68(%rbp), %eax
+	movl	%eax, %edx
+	leaq	-64(%rbp), %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	my_reverse
+	nop
+	movq	-8(%rbp), %rax
+	xorq	%fs:40, %rax
+	je	.L2
+	call	__stack_chk_fail
+.L2:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
--- project1/sources.mk
+++ project1/sources.mk
@@ -1,4 +1,3 @@
-
 # Files
 src=test.c memory.c memory.h
 i_files= test.i memory.i
@@ -8,9 +7,11 @@ s_files= test.s memory.s
 #         Directories     
 objdir=
 predir=processed_files
-assmdir=assembled_files
+asmdir=assembled_files
+objdir=object_files
 
 vpath %.c source
-vpath %.h source
+vpath %.h headers
 vpath %.i processed_files
-
+vpath %.s assembled_files
+vpath %.o object_files
