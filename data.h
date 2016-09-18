4405af740c28cdc99e07b5de64904666aa1a110f
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-17 23:31:00 -0600

    Updtaed data.h file

--- data.h
+++ data.h
@@ -1,9 +1,9 @@
 #ifndef _DATA_H
-
 #define _DATA_H
 
-int my_atoi(char *str);
+int32_t my_atoi(int8_t *str);
 void dump_memory(uint8_t *start, uint32_t length);
 uint32_t big_to_little(uint32_t data);
 uint32_t little_to_big(uint32_t data);
+
 #endif
