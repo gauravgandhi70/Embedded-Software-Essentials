e79837d0ae33a4f59d67e6f286db5d1aebada7ae
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-18 04:17:20 -0600

    Added comments to memory.h file

--- project1/source/test.c
+++ project1/source/test.c
@@ -4,19 +4,6 @@
 
 void main()
 {
-    uint32_t length;
-    uint8_t source[50],destination[50];
-    
-    printf("enter the array \t");
-    scanf ("%[^\n]%*c",source);
-
-    printf("Enter the number of bytes that you want to reverse \t");
-    scanf("%d",&length);
-    
-    my_memmove(source, destination, length);
-    my_memzero(source, length);
-    my_reverse(source, length);
-
     uint32_t length;                                                             
     uint8_t source[50],destination[50];
 
