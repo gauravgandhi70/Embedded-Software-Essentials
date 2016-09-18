0e549bd09f76c8193180a951effae9b6a8bd7497
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-17 23:49:40 -0600

    Updated memory.c file

--- memory.c
+++ memory.c
@@ -8,67 +8,79 @@ int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length)
     uint8_t temp[50];
 
     if(src)
-        {
+    {
  
-            copying_LOOP(i,length,src,temp);
-            
-            i=0;
-            
-            copying_LOOP(i,length,temp,dst);
+        copying_LOOP(i,length,src,temp);
+        i=0;
+        copying_LOOP(i,length,temp,dst);
 
-            printf(" \n Source is at : %p new string at destination %p \t",src,dst);
-            i=0;
-
-            while(i<length)
-		  {
-			   printf("%c",*(dst+i));
-                           i++;
-		  }
-            return 0;
+        printf(" \n Source is at : %p new string at destination %p \t",src,dst);
+            
+        i=0;
+        while(i<length)
+        {
+            printf("%c",*(dst+i));
+            i++;
         }
+            
+        return 0;
+    }
+    
     else
-       {
-            printf("Pointer ERROR");
-            return 1;
-       }
+    {
+        printf("Pointer ERROR");
+        return 1;
+    }
 
 }
 
 int8_t my_memzero(uint8_t *src, uint32_t length)
 {
-    uint16_t i=0,len=0;
-	while(*(src+i)!='\0')
-	{
-		len++;
-		i++;
-	}
-	i=0;
-    if(src)
-      {
-        while(i<length)
-         {
+   uint16_t i=0,len=0;
+   while(*(src+i)!='\0')
+   {
+       len++;
+       i++;
+   }
+	
+   i=0;
+   if(src)
+   {
+       while(i<length)
+       {
            *(src+i)=0;
            i++;
-         }
-        i=0;
-        printf(" \n New String is \t");
-	    while(i<len)
- 	     { 
-		   printf("%c",*(src+i));
-		   i++;
-	     }
-        
-        printf("\n"); 
-
-        return 0;
        }
+        
+       i=0;
+       printf(" \n New String is \t");
+       while(i<len)
+       { 
+           printf("%c",*(src+i));
+           i++;
+       }
+        
+       printf("\n"); 
+
+       return 0;
+   }
 	 
-    else
-	   {
-         printf("Pointer Error");
-         return 1;
-       } 
+   else
+   {
+       printf("Pointer Error");
+       return 1;
+   } 
 }
+
+
+
+
+
+
+
+
+
+
 
 int8_t my_reverse(char *src, int length)
 {
