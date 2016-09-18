e2a82e8e7b80c36001067119c2a8a43415779a68
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-09-17 23:36:42 -0600

    Updated makefile and added compile-all target. Where you can compile either one or all files. Also increased scope of dependancies so that .s files depends on .c and .s files depend on .i while .o files depend upon .s files

--- project1/sources.mk
+++ project1/sources.mk
@@ -1,17 +1,19 @@
-# Files
-src=test.c memory.c memory.h
-i_files= test.i memory.i
-s_files= test.s memory.s
-
-
-#         Directories     
-objdir=
-predir=processed_files
-asmdir=assembled_files
-objdir=object_files
-
 vpath %.c source
 vpath %.h headers
 vpath %.i processed_files
 vpath %.s assembled_files
 vpath %.o object_files
+
+# Files
+src=test.c memory.c memory.h
+i_files= test.i memory.i
+s_files= test.s memory.s
+obj_files= test.o memory.o
+
+
+#         Directories     
+predir=processed_files
+asmdir=assembled_files
+objdir=object_files
+
+
