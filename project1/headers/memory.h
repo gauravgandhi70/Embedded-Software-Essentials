e6d4d24a246c72c45ee8a0ce66ae4337a4162e1c
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-18 04:17:20 -0600

    Added comments to memory.h file

--- project1/headers/memory.h
+++ project1/headers/memory.h
@@ -2,9 +2,9 @@
 #define _HW1_H
 
 
-int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length);
-int8_t my_memzero(uint8_t *src, uint32_t length);
-int8_t my_reverse(char *src, int length);
+int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length);   //Function prototype for transferring data from one location to another
+int8_t my_memzero(uint8_t *src, uint32_t length);                 //Function prototype to set given length of bytes to zero
+int8_t my_reverse(char *src, int length);                         //Function prototype to reverse 
 
 
 #endif
