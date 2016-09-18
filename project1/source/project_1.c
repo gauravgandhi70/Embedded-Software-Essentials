526e02a91c92e1238cfe4a2c468f2820c2974aad
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-18 17:50:54 -0600

    Created project_1.c file containing the function project_1_report

--- project1/source/project_1.c
+++ project1/source/project_1.c
@@ -1,11 +1,35 @@
 #include<stdio.h>
-
+#include<stdint.h>
+#include"memory.h"
+#include"project_1.h"
 
 void project_1_report()
 {
 
     uint8_t array[32];
-    uint8_t *aptr_1, *aptr_2, *aptr_3;
-    *aptr_1=array[0];
-    *aptr_2=array[8];
-    *aptr_3=ar
+    uint8_t *aptr_1=null, *aptr_2=null , *aptr_3= null;
+    uint32_t i,data=31;
+    aptr_1=(array);
+    aptr_2=(array+8);
+    aptr_3=(array+16);
+
+    for(i=0;i<=15;i++)
+    {
+    *(aptr_1 + i)=data;
+    data++;
+    }
+
+    my_memzero(*aptr_3, 16);
+ 
+    my_memmove(*aptr_1, *aptr_3, 8);
+
+    my_memmove(*aptr_2. *aptr_1, 16);
+
+    my_reverse(*aptr_1, 32);
+
+    for(i=0;i<31;i++)
+    {
+    printf("The value in byte %d is:%d\n",i,*(array+i);
+    }
+
+    
