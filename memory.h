70d72181e5e11800944c3411b19c50c1088ce939
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-18 00:26:33 -0600

    Updated memory.h file

--- memory.h
+++ memory.h
@@ -6,8 +6,8 @@ while(start<endpoint)                                           \
     *(copy_dest+start)=*(original_data+start);                  \
     start++;                                                    \
 }
-int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length);
 
+int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length);
 int8_t my_memzero(uint8_t *src, uint32_t length);
 int8_t my_reverse(char *src, int length);
 
