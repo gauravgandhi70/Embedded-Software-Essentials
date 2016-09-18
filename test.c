4cdddee4189b422eb77fcbc932075f22b7d68b06
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-18 00:24:00 -0600

    Updated test.c file

--- test.c
+++ test.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 #include<stdint.h>
 #include"memory.h"
-int8_t my_reverse(char *src, int length);
+
 void main()
 {
     uint32_t length;
-
-	uint8_t source[50],destination[50];
-	
-	    printf("enter the array \t");
-		scanf ("%[^\n]%*c",source);
+    uint8_t source[50],destination[50];
+    
+    printf("enter the array \t");
+    scanf ("%[^\n]%*c",source);
 
     printf("Enter the number of bytes that you want to reverse \t");
     scanf("%d",&length);
+    
     my_reverse(source,length);
+    int8_t my_memmove(source, destination, length);
+    int8_t my_memzero(source, length);
+    int8_t my_reverse(source, length);
 }
 
 
