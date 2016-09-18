30a9abb7d7a48d95321b296f7c9baf036cb0c55d
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-09-18 16:36:45 -0600

    Moved all the source files into source subdir

--- project1/source/data.c
+++ project1/source/data.c
@@ -1,6 +1,9 @@
 #include<stdint.h>
 #include"data.h"
 
+
+void reverse(int8_t *str, int32_t length);                //Function declaration for reverse
+
 int32_t my_atoi(int8_t *str)                              //Function definition for ascii string to integer function
 {
    printf("string:%s\n",str);                             //Prints the input string
@@ -24,7 +27,7 @@ void dump_memory(uint8_t *start, uint32_t length)         //Function definition 
    int32_t i;
    for(i=0;i<length;i++)                                  
    {
-      printf("\n Hex otuput: %x\t",*(start+i));                          //Prints the hex value of number of bytes given in the length
+      printf("\n Hex output: %x\t",*(start+i));           //Prints the hex value of number of bytes given in the length
 	
 	
    }
@@ -64,3 +67,77 @@ uint32_t little_to_big(uint32_t data)                    //Function definition f
    else
        printf("The data is already stored as big endian\n");
 }
+
+int8_t* itoa(int32_t num, int8_t* str, int32_t base)      //Function definition for converting data from integer to ascii string
+{
+    int32_t i = 0;
+    int32_t neg=0;
+ 
+                                                          // Handle 0 explicitely, otherwise empty string is printed for 0
+    printf("the number is:%d\n",num);
+    if (num ==0)
+    {
+        *str='0';
+        i++;
+		*(str+i)='\0';
+        printf("The string is:%s",str);
+        return str;
+    }
+ 
+    if(num<0 && base==16) 
+    {
+        sprintf(str,"%X",num);                                           // convert decimal to hexadecimal 
+        printf("converting %d to hexadecimal notation %s\n",num,str);    //shows the hex output for signed integer
+        return str;
+    }
+	
+    if(num<0 && base==8) 
+    {
+        sprintf(str,"%o",num);                                         //convert decimal to octal  
+        printf("converting %d to octal notation %s\n",num,str);        //shows the octal output of signed integer
+        return str;
+    }
+    
+    if (num < 0 && (base == 10 || base==2))
+    {
+        neg = 1;                                                       //Set neg varibale to 1 if the number is negative
+        num = -num;                                                    //Consider only unsigned number initially for conversion
+    }
+ 
+                                                                       // Process individual digits
+    while (num != 0)
+    {
+        int32_t rem = num % base;
+        *(str+i)= (rem > 9)?(rem-10) + 'a' : rem + '0';               //Converting integer to ascii string
+        i++;
+        num = num/base;
+    }
+    
+    if (neg==1)                                                        //If number is negative, append '-'
+    {
+        *(str+i)= '-';
+        i++;
+    }
+ 
+    *(str+i)= '\0';                                                    // Append string terminator
+    reverse(str, i);                                                   // Reverse the string
+    printf("The string is:%s",str);                                    //Print the ascii string
+ 
+    return str;
+}
+
+void reverse(int8_t *str, int32_t length)                             //Function to perform reverse of the string
+{
+    int32_t start = 0;
+    int32_t end = length -1;
+    while (start < end)                                               //Swap the string
+    {
+        int8_t temp= *(str+start);
+        *(str+start)= *(str+end);
+        *(str+end)=temp;
+        start++;
+        end--;
+    }
+}
+
+
