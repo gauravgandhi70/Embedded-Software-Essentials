fe44da3b659b6bb6ae2983f6131ce33502e8e8b4
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-09-18 19:36:40 -0600

    Resolving merge conflict

--- project1/sources.mk
+++ project1/sources.mk
@@ -4,7 +4,6 @@ vpath %.h headers
 vpath %.i processed
 vpath %.s assembled
 vpath %.o object
-obj_path= object/test.o object/memory.o
 include= headers
 
 
@@ -13,6 +12,12 @@ src=memory.c project_1.c main.c
 i_files= memory.i project_1.i main.i 
 s_files= memory.s project_1.s main.s
 o_files= memory.o project_1.o main.o 
+
+src=main.c memory.c data.c project_1.c
+i_files= main.i memory.i data.i project_1.i
+s_files= main.s memory.s data.c project_1.s
+o_files= main.o memory.o data.o project_1.o
+
 
 
 
