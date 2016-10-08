769b9f6c63f4242ccce4fe0e45fc61b1c7022d37
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-10-08 01:13:58 -0600

    Added memory.c
    Used my_memzero function for implementing the echo requirement in the
    UART logger

---
+++ Project2/Includes/memory.h
@@ -0,0 +1,15 @@
+/*
+ * memory.h
+ *
+ *  Created on: 08-Oct-2016
+ *      Author: Admin
+ */
+
+#ifndef INCLUDES_MEMORY_H_
+#define INCLUDES_MEMORY_H_
+
+int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length);   //Function prototype for transferring data from one location to another
+int8_t my_memzero(uint8_t *src, uint32_t length);                 //Function prototype to set given length of bytes to zero
+int8_t my_reverse(uint8_t *src, uint32_t length);                         //Function prototype to reverse
+
+#endif /* INCLUDES_MEMORY_H_ */
---
+++ Project2/Sources/memory.c
@@ -0,0 +1,118 @@
+/*
+ * memory.c
+ *
+ *  Created on: 08-Oct-2016
+ *      Author: Admin
+ */
+#include "MKL25Z4.h"
+#include"memory.h"
+
+
+int8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length)                               //Function definition to move data from one location to other location
+{
+    uint32_t i=0;
+    uint8_t temp[50];
+
+    if(src)
+    {
+
+
+
+        while(i<length)                                                                     //Copies data from source  to temp array
+        {
+             *(temp+i)=*(src+i);
+             i++;
+        }
+        i=0;
+
+        while(i<length)                                                                       //Copies data from temp to destination array
+        {
+             *(dst+i)=*(temp+i);
+             i++;
+        }
+
+
+
+
+
+        return 0;
+
+
+    }
+
+    else
+    {
+
+                                                                  //Displays an error if move failed
+        return 1;
+    }
+
+
+
+
+}
+
+int8_t my_memzero(uint8_t *src, uint32_t length)                                             //Function definition to zero out given length of bytes in the memory
+{
+
+    uint16_t i=0,len=0;
+    while(*(src+i)!='\0')
+    {
+        len++;                                                                               //Calculates the total length of the memory
+        i++;
+    }
+
+    i=0;
+
+    if(src)
+    {
+        while(i<length)
+        {
+             *(src+i)=0;                                                                     //Clears the memory i.e sets it to zero for given length of bytes
+             i++;
+        }
+
+        i=0;
+
+
+
+        return 0;
+    }
+
+    else
+    {
+
+        return 1;
+    }
+
+
+}
+
+int8_t my_reverse(uint8_t *src, uint32_t length)                                                     //Function definition for reverse function
+{
+    uint16_t counter;
+
+    if(src)
+    {
+        int8_t temp=0;
+        for (counter=0;counter<(length/2);counter++)
+        {
+           temp=*(src+counter);                                                             //Performs the reverse operation  using a temporary variable
+           *(src+counter)=*(src+length-counter-1);
+           *(src+length-counter-1)=temp;
+        }
+
+
+        return 0;
+    }
+
+    else
+    {
+        return 1;
+    }
+
+}
+
+
+
+
