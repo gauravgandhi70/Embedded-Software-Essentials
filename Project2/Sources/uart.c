d6b61527a3da43ae8fc4010dedb29559ee590244
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-10-09 00:42:21 -0600

    Clock configuration comments added

--- Project2/Sources/uart.c
+++ Project2/Sources/uart.c
@@ -12,46 +12,50 @@
 
 void uart_init(uint32_t baud)
 {
-	uint16_t sbr;
+	uint16_t baud_divisor;
 	uint8_t temp;
 
-	//Set clock frequency for PORTA
+	//Set gate clock  for PORTA
 	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 
-	//Set clock frequency
+	//Enable clock for UART0 and set clock source as MCGCLKFLL
 	SIM_SCGC4 |= SIM_SCGC4_UART0_MASK;
-	SIM_SOPT2 &= ~SIM_SOPT2_UART0SRC_MASK;
-	SIM_SOPT2 |= SIM_SOPT2_UART0SRC(1);// Select the clock as PLL/2
+	SIM_SOPT2 |= SIM_SOPT2_UART0SRC(1);
 
-	PORTA_PCR1 = PORT_PCR_MUX(2);// Set the alternate function for PORTA
+	// Set the alternate function for PORTA as UART0
+	PORTA_PCR1 = PORT_PCR_MUX(2);
 	PORTA_PCR2 = PORT_PCR_MUX(2);
 
-	UART0_C2_REG(UART0_BASE_PTR) &= ~(UART_C2_TE_MASK | UART_C2_RE_MASK); // Switch off UART before setting
-	UART0_C1_REG(UART0_BASE_PTR)=0x00; //Set all default configuration
+	// Disable tx and rx before editing registers of UART0
+	UART0_C2=0x00;
+	UART0_C1=0x00;
 
-	//SBR calculation
 
-	sbr= (uint16_t)(DEFAULT_SYSTEM_CLOCK/(baud *16));
 
-	/* Save off the current value of the uartx_BDH except for the SBR field */
-	temp = UART_BDH_REG(UART0_BASE_PTR) & ~(UART_BDH_SBR(0x1F));
-	UART_BDH_REG(UART0_BASE_PTR) = temp |  UART_BDH_SBR(((sbr & 0x1F00) >> 8));//set SBR field for Baud rate
-	UART_BDL_REG(UART0_BASE_PTR) = (uint8_t)(sbr & UART_BDL_SBR_MASK);
 
+
+	/* Calculate baud_divisor for sbr register and set baud rate */
+	baud_divisor= (uint16_t)(DEFAULT_SYSTEM_CLOCK/(baud *16));
+	UART0_BDH |= (baud_divisor >> 8) & UART0_BDH_SBR_MASK;
+	UART0_BDL = (baud_divisor & UART0_BDL_SBR_MASK);
+
+
+	//tx buffer initialization
 	tx_buf.buff = initialize(&tx_buf);
 	tx_buf.head=tx_buf.data;
 			 	tx_buf.tail=tx_buf.data;
 			 	tx_buf.buff=tx_buf.data;
 			 	tx_buf.new_len= 0;
-    rx_buf.buff = initialize(&rx_buf);
+
+	//rx buffer initialization
+	rx_buf.buff = initialize(&rx_buf);
     rx_buf.head=rx_buf.data;
 			rx_buf.tail=rx_buf.data;
 			rx_buf.buff=rx_buf.data;
 			rx_buf.new_len= 0;
-	//Set NVIC interrupts and set priority
-	NVIC_EnableIRQ(UART0_IRQn);
-	NVIC_SetPriority(UART0_IRQn,2);
 
+	//Set NVIC interrupts and CPU interrupts
+	NVIC_EnableIRQ(UART0_IRQn);
 	UART_C2_REG(UART0_BASE_PTR) |= UART_C2_RIE_MASK; //Setup receiver interrupt
 	//UART_C2_REG(UART0_BASE_PTR) |= UART_C2_TIE_MASK;
 	__enable_irq();
