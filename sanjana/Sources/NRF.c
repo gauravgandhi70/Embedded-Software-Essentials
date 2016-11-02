f3b7dbd46587f9462bc46e92d3c763b30291c967
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2016-11-02 13:30:17 -0600

    Added comments to NRF and SPI ,C files

--- sanjana/Sources/NRF.c
+++ sanjana/Sources/NRF.c
@@ -5,39 +5,39 @@
 
 void nrf_config_write()
 {
-	PTC_BASE_PTR->PCOR = 1<<4;
+	PTC_BASE_PTR->PCOR = 1<<4; //Chip select low
 	//PTC->PCOR=
-	nrf_write_register(NORDIC_CONFIG_REG);
-	SPI_write(0x03);
-	PTC_BASE_PTR->PSOR =  1<<4;
+	nrf_write_register(NORDIC_CONFIG_REG); //write command to Config register
+	SPI_write(0x03);//Write data to Config register
+	PTC_BASE_PTR->PSOR =  1<<4; //Chip select high
 }
 
 void nrf_config_read()
 {
-	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; //enable clock to port B
 	char c;
-	PTC_BASE_PTR->PCOR = 1<<4;
-	nrf_read_register(NORDIC_CONFIG_REG);
-	c=SPI_write(0XFF);
+	PTC_BASE_PTR->PCOR = 1<<4; //Chip select low
+	nrf_read_register(NORDIC_CONFIG_REG); // read command to Config register
+	c=SPI_write(0XFF);//Send dummy value to recieve data
 	//c=SPI_read();
-	if (c==0x03)
+	if (c==0x03) //if data recieved is data sent, glow RED LED
 	{   LED_Init();
 	    LEDFunction(RED, 500);
 	   /* PTB_BASE_PTR->PDOR |= 1<<18;
 		PTB_BASE_PTR->PDDR |= 1<<18;
 		PTB_BASE_PTR->PSOR = 1<<18;*/
 	}
-	PTC_BASE_PTR->PSOR = 1<<4;
+	PTC_BASE_PTR->PSOR = 1<<4; //Chip select high
 }
 
 void nrf_write_register(unsigned char address)
 {
-	SPI_write(0x20|address);
+	SPI_write(0x20|address); //write command
 }
 
 void nrf_read_register(unsigned char address)
 {
-	SPI_write(0x00|address);
+	SPI_write(0x00|address);//read command
 }
 
 /*void nrf_flush_tx_fifo()
@@ -66,7 +66,7 @@ void nrf_status_read()
 
 void nrf_fifostatus_read()
 {
-	
+
 	char c;
 	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(FIFO_STATUS_REG );
--- sanjana/Sources/SPI.c
+++ sanjana/Sources/SPI.c
@@ -3,29 +3,29 @@
 
 void SPI_init(void) {
 	// Enable clock network to SPI0
-	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-	SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;
-	PTC_BASE_PTR->PDDR |= 0x10;
-	PTC_BASE_PTR->PDOR |= 0x10;
-	
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;// Clock get to PORT C
+	SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;//Clock get to SPI0
+	PTC_BASE_PTR->PDDR |= 0x10; //Set pin 4 port C i.e PCS as output direction
+	PTC_BASE_PTR->PDOR |= 0x10; //Set pin 4 port C i.e PCS as output
+
 	// configure output crossbar
 	PORTC->PCR[4] = PORT_PCR_MUX(1);  // PCS
 	PORTC->PCR[5] = PORT_PCR_MUX(2);  // SCK
 	PORTC->PCR[6] = PORT_PCR_MUX(2);  // MOSI
 	PORTC->PCR[7] = PORT_PCR_MUX(2);  // MISO
-	
+
 	// configure gpio address select
 	/* here */
-	
+
 	// Config registers, turn on SPI0 as master
 	// Enable chip select
-	SPI0->C1 = 0x50;
+	SPI0->C1 = 0x50;// Enable SPI and set as Master
 	//SPI0->C2 = 0x10;
 	//SPI0->BR = 0x00;
 	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));     //Set baud rate prescale divisor to 3 & set baud rate divisor to 512 for baud rate of 15625 hz
 }
 
-uint8_t SPI_status(void) {
+uint8_t SPI_status(void) { //Shows the status of SPI
 	return SPI0->S;
 }
 
@@ -34,18 +34,18 @@ uint8_t SPI_write(uint8_t p) {
 	int i;
 	// set SPI line to output (BIDROE = 1)
 	//SPI0->C2 |= 0x04;
-	
+
 	//for (i = 0; i < size; ++i) {
 		// poll until empty
-		while ((SPI_status() & 0x20) != 0x20);
-		SPI0->D = p;
-		while ((SPI_status() & 0x20) != 0x20);
-		return SPI0->D;
+		while ((SPI_status() & 0x20) != 0x20); //Check if transfer buffer is empty
+		SPI0->D = p;//Send data
+		while ((SPI_status() & 0x20) != 0x20); //Check if transfer is complete, hence transfer buffer empty
+		return SPI0->D; //return data
 	//}
 }
 
 // Read size number of characters into buffer p from register at address
-  uint8_t SPI_read() {
+  /*uint8_t SPI_read() {
 	int i;
 	uint8_t p;
 
@@ -56,11 +56,11 @@ uint8_t SPI_write(uint8_t p) {
 		//SPI0->D = 0x00;
 		while ((SPI_status() & 0x80) != 0x80);
 		p = SPI0->D;
-		return p;
+		return p;*/
 	//}
 }
 
-void spi_flush()
+void spi_flush() //To flush out the transmit and recieve buffers
 {
 	SPI0_C1&=0xBF;
 	SPI_init();
