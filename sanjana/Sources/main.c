025d69f04abe9241a1ef75928179fc0f947c0989
SanjanaVK <saka2821@colorado.edu>
2016-11-05 00:22:32 -0600

    Called all the functions required in the main

--- sanjana/Sources/main.c
+++ sanjana/Sources/main.c
@@ -11,6 +11,11 @@ int main(void)
 
 		  nrf_config_write();
 		  nrf_config_read();
-
-	  }
+		  nrf_status_read();
+		  nrf_fifostatus_read();
+		  nrf_rfsetup_transmit();
+		  nrf_rfsetup_receive();
+		  nrf_tx_addr_write();
+		  nrf_tx_addr_read();
+     }
 }
