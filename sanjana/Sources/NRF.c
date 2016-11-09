78fcab3a3c51cfc6ef852e25fe19b9d8ac6579c2
SanjanaVK <saka2821@colorado.edu>
2016-11-09 10:13:10 -0700

    added comments to the functions

--- sanjana/Sources/NRF.c
+++ sanjana/Sources/NRF.c
@@ -2,13 +2,10 @@
 #include "NRF.h"
 #include "SPI.h"
 #include "led.h"
-#include "uart.h"
-#include "log.h"
 
 void nrf_config_write()
 {
 	PTC_BASE_PTR->PCOR = 1<<4; //Chip select low
-	//PTC->PCOR=
 	nrf_write_register(NORDIC_CONFIG_REG); //write command to Config register
 	SPI_write(0x03);//Write data to Config register
 	PTC_BASE_PTR->PSOR =  1<<4; //Chip select high
@@ -17,17 +14,13 @@ void nrf_config_write()
 void nrf_config_read()
 {
 	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; //enable clock to port B
-	char c;
+	uint8_t c;
 	PTC_BASE_PTR->PCOR = 1<<4; //Chip select low
 	nrf_read_register(NORDIC_CONFIG_REG); // read command to Config register
 	c=SPI_write(0XFF);//Send dummy value to recieve data
-	//c=SPI_read();
 	if (c==0x03) //if data recieved is data sent, glow RED LED
 	{   LED_Init();
 	    LEDFunction(RED, 500);
-	   /* PTB_BASE_PTR->PDOR |= 1<<18;
-		PTB_BASE_PTR->PDDR |= 1<<18;
-		PTB_BASE_PTR->PSOR = 1<<18;*/
 	}
 	PTC_BASE_PTR->PSOR = 1<<4; //Chip select high
 }
@@ -42,65 +35,58 @@ void nrf_read_register(unsigned char address)
 	SPI_write(0x00|address);//read command
 }
 
-void nrf_flush_tx_fifo()
+void nrf_flush_tx_fifo() //function to flush the tx fifo
 {
 	PTC_BASE_PTR->PCOR = 1<<4;
 	SPI_write(0xE1);
 	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
-void nrf_flush_rx_fifo()
+void nrf_flush_rx_fifo() //function to flush the rx fifo
 {
 	PTC_BASE_PTR->PCOR = 1<<4;
 	SPI_write(0xE2);
 	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
-void nrf_status_read()
+void nrf_status_read() //function to read the nrf status register
 {
-	char c;
+	uint8_t c;
 	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(NORDIC_STATUS_REG);
-	c=SPI_write(0xFF);
-
+	SPI_write(0xFF);
+	c=SPI_read();
 	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
-void nrf_fifostatus_read()
+void nrf_fifostatus_read() //function to read the nrf fifostatus register
 {
 
-	char c;
+	uint8_t c;
 	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(FIFO_STATUS_REG );
 	c=SPI_write(0xff);
-	// c=SPI_read();
 	 PTC_BASE_PTR->PSOR = 1<<4;
 }
 
 void nrf_transmit_data() //send data to nrf buffer
 {
-	PTD_BASE_PTR->PCOR = 1<<0;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	SPI_write(W_TXPAYLOAD);
 	SPI_write(0x10);
-	PTD_BASE_PTR->PSOR = 1<<0;
+	PTC_BASE_PTR->PSOR = 1<<4;
 }
  
 void nrf_read_data() //receive data from nrf buffer
 {
-	char c;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint8_t c;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	SPI_write(R_RXPAYLOAD);
-	SPI_write(0xff);
-	c=SPI_read();
-	PTD_BASE_PTR->PSOR = 1<<0;
+	c=SPI_write(0xff);
+	PTC_BASE_PTR->PSOR = 1<<4;
 
 	nrf_fifostatus_read();
 
-	if (c==0x07) //poll for data 07 from the tx
-	{
-		PTB_BASE_PTR->PDDR |= 1<<18;
-
-	}
 }
 
 void nrf_rfsetup_transmit() //setup the rf register
@@ -115,32 +101,29 @@ void nrf_rfsetup_receive() //read the rf register
 {
 	uint8_t c;
 	PTC_BASE_PTR->PCOR = 1<<4;
-	nrf_read_register(NORDIC_RF_SETUP_REG);
+	nrf_write_register(NORDIC_RF_SETUP_REG);
 	c=SPI_write(0xff);
-	//LOG0(&c);
-
 	PTC_BASE_PTR->PSOR = 1<<4;
 
 }
 
 void nrf_tx_addr_read() //read the tx address register
 {
-	int i;
-	char c;
+	uint32_t i;
+	uint8_t c;
 	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(TX_ADDR);
 	 for(i=0;i<5;i++)
 	 {
 
 		 c=SPI_write(0xff);
-
 	 }
 	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
 void nrf_tx_addr_write() //write the tx address register
 {
-	int i;
+	uint32_t i;
 	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_write_register(TX_ADDR);
 	 for(i=0;i<5;i++)
@@ -152,75 +135,72 @@ void nrf_tx_addr_write() //write the tx address register
 
 void nrf_rx_addr_read() //read the rx address register
 {
-	int i;
-	char c;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint32_t i;
+	uint8_t c;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(RX_ADDR_P0_REG);
 	 for(i=0;i<5;i++)
 	 {
-		 SPI_write(0xff);
-		 c=SPI_read();
+		 c=SPI_write(0xff);
 	 }
-	PTD_BASE_PTR->PSOR = 1<<0;
+    PTC_BASE_PTR->PSOR = 1<<4;
 }
 
 void nrf_rx_addr_write() //write the rx address register
 {
-	int i;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint32_t i;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_write_register(RX_ADDR_P0_REG);
 	 for(i=0;i<5;i++)
 	 {
 		 SPI_write(0xE7);
 	 }
-	 PTD_BASE_PTR->PSOR = 1<<0;
+	 PTC_BASE_PTR->PSOR = 1<<4;
 }
 
 void nrf_en_rxaddr_write() //write to the datapipe register
 {
-	int i;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint32_t i;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_write_register(NRF_ENRXADDR);
 
 	SPI_write(0x01);
 
-	 PTD_BASE_PTR->PSOR = 1<<0;
+	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
 void nrf_en_rxaddr_read() //read from the datapipe register
 {
-	int i;
-	char c;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint32_t i;
+	uint8_t c;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(NRF_ENRXADDR);
 
-	 SPI_write(0xff);
-	 c=SPI_read();
+	c=SPI_write(0xff);
 
-	 PTD_BASE_PTR->PSOR = 1<<0;
+	PTC_BASE_PTR->PCOR = 1<<4;
 }
 
 
 void nrf_rx_pipesize_write() //set the data pipe size
 {
-	int i;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint32_t i;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_write_register(NRF_PIPESIZE);
 
 	SPI_write(0x01);
 
-	PTD_BASE_PTR->PSOR = 1<<0;
+	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
 void nrf_rx_pipesize_read()  //read the data pipe size
 {
-	int i;
-	char c;
-	PTD_BASE_PTR->PCOR = 1<<0;
+	uint32_t i;
+	uint8_t c;
+	PTC_BASE_PTR->PCOR = 1<<4;
 	nrf_read_register(NRF_PIPESIZE);
 
-	 SPI_write(0xff);
-	 c=SPI_read();
+	 c=SPI_write(0xff);
 
-	 PTD_BASE_PTR->PSOR = 1<<0;
+	PTC_BASE_PTR->PSOR = 1<<4;
 }
--- sanjana/Sources/SPI.c
+++ sanjana/Sources/SPI.c
@@ -14,15 +14,13 @@ void SPI_init(void) {
 	PORTC->PCR[6] = PORT_PCR_MUX(2);  // MOSI
 	PORTC->PCR[7] = PORT_PCR_MUX(2);  // MISO
 
-	PTC_BASE_PTR->PSOR = 1<<4;
+	PTC_BASE_PTR->PSOR = 1<<4;//make gpio chip high
 	// configure gpio address select
 	/* here */
 
 	// Config registers, turn on SPI0 as master
 	// Enable chip select
 	SPI0->C1 = 0x50;// Enable SPI and set as Master
-	//SPI0->C2 = 0x10;
-	//SPI0->BR = 0x00;
 	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));     //Set baud rate prescale divisor to 3 & set baud rate divisor to 512 for baud rate of 15625 hz
 }
 
@@ -32,34 +30,12 @@ uint8_t SPI_status(void) { //Shows the status of SPI
 
 // Write out all characters in supplied buffer to register at address
 uint8_t SPI_write(uint8_t p) {
-	int i;
-	// set SPI line to output (BIDROE = 1)
-	//SPI0->C2 |= 0x04;
-
-	//for (i = 0; i < size; ++i) {
-		// poll until empty
-		while ((SPI_status() & 0x20) != 0x20); //Check if transfer buffer is empty
+        while ((SPI_status() & 0x20) != 0x20); //Check if transfer buffer is empty
 		SPI0->D = p;//Send data
-		while ((SPI_status() & 0x80) != 0x80); //Check if transfer is complete, hence transfer buffer empty
+		while ((SPI_status() & 0x80) != 0x80); //Check if transfer is complete, hence recieve buffer full
 		return SPI0->D; //return data
-	//}
 }
 
-// Read size number of characters into buffer p from register at address
-  /*uint8_t SPI_read() {
-	int i;
-	uint8_t p;
-
-	// set SPI line to input (BIDROE = 0)
-	//SPI0->C2 &= 0xF7;
-	//for (i = 0; i < size; ++i) {
-		// poll until full
-		//SPI0->D = 0x00;
-		while ((SPI_status() & 0x80) != 0x80);
-		p = SPI0->D;
-		return p;*/
-	//}
-//}
 
 void spi_flush() //To flush out the transmit and recieve buffers
 {
